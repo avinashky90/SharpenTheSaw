121e1567d86383fb2121b702cb825812cac152ae
ayadav <avinashkumar.yadav@lowes.com>
2018-10-02 11:52:08 -0400

    Recursive factorial methods...

--- RecursiveProjects/src/Factorial.java
+++ RecursiveProjects/src/Factorial.java
@@ -1,10 +1,20 @@
 
 public class Factorial {
+	
+	private static int num = 5;
 
+	/** Main Method
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		System.out.println(getFactorial(5));
+		System.out.println(getFactorial(num));
 	}
 
+	/**
+	 * Method generates Factorial.. 
+	 * @param i
+	 * @return
+	 */
 	private static int getFactorial(int i) {
 		if (i == 0)
 			return 1;
